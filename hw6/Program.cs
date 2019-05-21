de2f1feefd49fd86fccfa1aa5c603db14274a8ec
Paul Moesher <36650592+paul-moesher@users.noreply.github.com>
2019-05-21 10:42:35 -0400

    Add files via upload

---
+++ hw6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cValue;
+            int fValue;
+            string input;
+            string C_or_F;
+                do
+                {
+                    Console.WriteLine("Enter Temperature you want to convert (numbers only) or type Exit to end: ");
+                    input = Console.ReadLine();
+                    if (input == "Exit")
+                    {
+                        break;
+                    }
+                     
+                Console.WriteLine("Do you want to convert to F or to C? Please only enter F or C: ");
+                     C_or_F = Console.ReadLine();
+                    if (C_or_F == "F")
+                    {
+                       cValue = Int32.Parse(input);
+                        fValue = (cValue * 9) / 5 + 32;
+                        Console.WriteLine("Temperature in F is: " + fValue + " Press enter to continue");
+                        Console.ReadLine();
+                    }
+                    if (C_or_F == "C")
+                    {
+                       fValue = Int32.Parse(input);
+                    cValue = (fValue - 32) * 5 / 9;
+                    Console.WriteLine("Temperature in C is: " + cValue + " Press enter to continue");
+                        Console.ReadLine();
+                    }
+
+                  
+             
+
+                }
+
+                    while (input != "Exit");
+        }
+    }
+}
